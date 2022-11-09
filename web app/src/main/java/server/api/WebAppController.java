0673bc65e7dce5cd821005cbaf149171914e0f06
= <=>
2022-11-09 21:44:39 +0400

    Added front-end

--- web app/src/main/java/server/api/WebAppController.java
+++ web app/src/main/java/server/api/WebAppController.java
@@ -1,23 +1,40 @@
 package server.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import server.api.dto.movie.MovieConverter;
+import server.api.dto.movie.MovieDTO;
+import server.api.dto.movie.MovieInDto;
 import server.business.MovieService;
 
-@RestController
-@RequestMapping("http://localhost:63342/web%20app/templates/index.html?_ijt=aumlrvam7bjhj3s2lrom9kaqj8&_ij_reload=RELOAD_ON_SAVE")
+import java.util.Collection;
+
+@Controller
 public class WebAppController {
 
     @Autowired
     MovieService movieService;
 
-    @GetMapping
-    public String viewHomePage(Model model) {
+    @RequestMapping("/")
+    public String index(Model model) {
         model.addAttribute("movies", movieService.readAll());
-        return "index.html";
+        return"index";
+    }
+
+    @GetMapping("/add-new-movie")
+    public String addNewMovie(Model model) {
+        MovieInDto movieInDto = new MovieInDto();
+        model.addAttribute("movie", movieInDto);
+        return "newmovie";
     }
 
 }
--- web app/src/main/java/server/api/dto/movie/MovieInDto.java
+++ web app/src/main/java/server/api/dto/movie/MovieInDto.java
@@ -2,9 +2,11 @@ package server.api.dto.movie;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MovieInDto {
     private String movieName;
     private int length;
